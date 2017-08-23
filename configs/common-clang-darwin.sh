fce7f5bd0ce3ae855cc86b369e8f6cbd1c2a655e
Nathan Toone <nathan@toonetown.com>
2017-08-23 07:41:10 -0600

    Add in optimization flag for clang and gcc

--- configs/common-clang-darwin.sh
+++ configs/common-clang-darwin.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 SDK_FLAG="${SDK_VERSION_NAME}-version-min=${MIN_OS_VERSION}"
+OPT_FLAG="${OPT_FLAG:--O3 -Wno-inline}"
 HOST="${HOST_ARCH}-apple-darwin"
 
 export CC="${CLANG_PATH}"
-export CFLAGS="-arch ${ARCH} -m${SDK_FLAG} -fPIC -fembed-bitcode -isysroot ${SDK_PATH}"
-export CPPFLAGS="-arch ${ARCH} -m${SDK_FLAG} -fPIC -fembed-bitcode -isysroot ${SDK_PATH}"
+export CPPFLAGS="-arch ${ARCH} -m${SDK_FLAG} -isysroot ${SDK_PATH} -fPIC -fembed-bitcode"
+export CFLAGS="${OPT_FLAG}"
+export CXXFLAGS="${OPT_FLAG}"
 export LDFLAGS="-arch ${ARCH} -m${SDK_FLAG} -isysroot ${SDK_PATH}"
--- configs/common-gcc-android.sh
+++ configs/common-gcc-android.sh
@@ -2,6 +2,7 @@
 
 PREBUILT_ROOT="${ANDROID_NDK_HOME}/toolchains/${GCC_ARCH}-${ANDROID_GCC_VERSION}/prebuilt/darwin-x86_64"
 PFIX="${PREBUILT_ROOT}/bin/${GCC_PREFIX}"
+OPT_FLAG="${OPT_FLAG:--O3 -finline-functions -Wno-inline}"
 HOST="arm-linux-androideabi"
 
 export SYSROOT="${ANDROID_NDK_HOME}/platforms/android-${ANDROID_PLATFORM}/arch-${PLATFORM_ARCH}"
@@ -15,17 +16,17 @@ export CPP="${PFIX}-cpp"
 export CXX="${PFIX}-g++"
 export LD="${PFIX}-ld"
 export STRIP="${PFIX}-strip"
-export CFLAGS="--sysroot=${CROSS_SYSROOT} ${COMP_FLAGS}"
 export CPPFLAGS="--sysroot=${CROSS_SYSROOT} -isystem${CXX_STL_ROOT}/include \
                                             -isystem${CXX_STL_ROOT}/libs/${CXX_STL_ARCH}/include \
                                             -isystem${CXX_STL_ROOT}/include/backward \
                                             ${COMP_FLAGS}"
+export CFLAGS="${OPT_FLAG}"
+export CXXFLAGS="${OPT_FLAG}"
 export LDFLAGS="--sysroot=${CROSS_SYSROOT} -L${CXX_STL_ROOT}/libs/${CXX_STL_ARCH}"
 export LIBS="-lgnustl_static" 
 
 ANDROID_BUILD_PIE="${ANDROID_BUILD_PIE:-true}"
 if [ "${ANDROID_BUILD_PIE}" == "true" ]; then
-    export CFLAGS="${CFLAGS} -fPIE"
     export CPPFLAGS="${CPPFLAGS} -fPIE"
     export LDFLAGS="${LDFLAGS} -fPIE -pie"
 fi
