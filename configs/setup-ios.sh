3c26e5cbee25bc28e0ae33807c126737e7771b69
Nathan Toone <nathan@toonetown.com>
2018-08-09 16:03:50 -0600

    Build targeting ios 9.0 or later

--- configs/setup-ios.sh
+++ configs/setup-ios.sh
@@ -4,4 +4,4 @@ LIPO_PATH="$(which lipo)"
 GEN_SCRIPT="${CONFIGS_DIR}/common-clang-darwin.sh"
 
 CLANG_PATH="$(xcrun -f clang)"
-MIN_OS_VERSION="${MIN_OS_VERSION:-8.0}"
+MIN_OS_VERSION="${MIN_OS_VERSION:-9.0}"
